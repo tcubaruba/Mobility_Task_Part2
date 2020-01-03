a25924abf814f96afb3f8ffb4d65034d4bba36bb
a1204151 <a1204151@unet.univie.ac.at>
2020-01-03 23:09:46 +0100

    function for getting scores added to utils

--- neural_networks/nn_SCM.py
+++ neural_networks/nn_SCM.py
@@ -1,30 +1,25 @@
 import pandas as pd
 import numpy as np
 import sys
-import preprocess.preprocess
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_validate
 from utils.models_utils import load_features_data
 from utils.models_utils import prepare_multiclass_target
+from utils.models_utils import get_scores_for_cross_val
+from preprocess.preprocess import normalize_X
 
 pd.set_option('display.max_columns', None)
 np.set_printoptions(threshold=sys.maxsize)
 
 X, y = load_features_data()
-
 y = prepare_multiclass_target(y)
 
 # normalize X
-X = preprocess.preprocess.normalize_X(X)
+X = normalize_X(X)
 
-model = MLPClassifier(hidden_layer_sizes=(100,50), activation='relu', solver='adam', verbose=False, max_iter=1000, alpha=0.001)
-                      #batch_size=64, alpha=0.001, early_stopping=True)
+model = MLPClassifier(hidden_layer_sizes=(200,100), activation='relu', solver='adam', verbose=False, max_iter=1000, alpha=0.001,
+                      batch_size=100, beta_1=0.99)
 
-scoring = ['accuracy', 'precision_macro', 'precision_weighted', 'recall_macro', 'recall_weighted', 'f1_macro',
-           'f1_weighted']
-scores = cross_validate(model, X, y, scoring=scoring,
-                        cv=5, return_train_score=True, n_jobs=-1)
-# print(scores)
+scores = get_scores_for_cross_val(model, X, y)
 for score in scores:
     print(score, ": ", scores[score])
 
--- utils/models_utils.py
+++ utils/models_utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from preprocess.preprocess import find_high_corr
 from preprocess.preprocess import target_to_numerical
 
+from sklearn.model_selection import cross_validate
+
 
 def load_features_data():
     features = pd.read_csv('../final/all_features.csv')
@@ -25,13 +27,23 @@ def load_features_data():
     X = X.set_index('key_col')
     return X, y
 
+
 def prepare_multiclass_target(y):
     y = target_to_numerical(y, 'mode')
     y = np.ravel(y)
     return y
 
+
 def prepare_binary_target(full_y, true_val):
     y = np.where(full_y == true_val, 1.0, 0.0)
     y = np.ravel(y)
     return y
+
+
+def get_scores_for_cross_val(model, X, y):
+    scoring = ['accuracy', 'precision_macro', 'precision_weighted', 'recall_macro', 'recall_weighted', 'f1_macro',
+               'f1_weighted']
+    scores = cross_validate(model, X, y, scoring=scoring,
+                            cv=5, return_train_score=True, n_jobs=-1)
+    return scores
 
