6b1c2e482b37986b9b75899dff13a7884d9d98d0
a1204151 <a1204151@unet.univie.ac.at>
2020-01-05 09:51:30 +0100

    added final metrics function

--- utils/models_utils.py
+++ utils/models_utils.py
@@ -5,6 +5,11 @@ from preprocess.preprocess import find_high_corr
 from preprocess.preprocess import target_to_numerical
 
 from sklearn.model_selection import cross_validate
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
 
 
 def load_features_data(correlation_threshold=0.1):
@@ -48,6 +53,20 @@ def get_scores_for_cross_val(model, X, y):
     scores = cross_validate(model, X, y, scoring=scoring,
                             cv=5, return_train_score=True, n_jobs=-1)
     return scores
+
+def get_final_metrics(y_true, y_pred):
+    scoring = {
+        "Accuracy": accuracy_score(y_true, y_pred),
+        "Precision macro": precision_score(y_true, y_pred, average='macro'),
+        "Precision weighted": precision_score(y_true, y_pred, average='weighted'),
+        "Recall macro": recall_score(y_true, y_pred, average='macro'),
+        "Recall weighted": recall_score(y_true, y_pred, average='weighted'),
+        "F1 macro": f1_score(y_true, y_pred, average='macro'),
+        "F1 weighted": f1_score(y_true, y_pred, average='weighted'),
+        "Confusion matrix": confusion_matrix(y_true, y_pred)
+    }
+    return scoring
+
 
 
 def count_class_members():
